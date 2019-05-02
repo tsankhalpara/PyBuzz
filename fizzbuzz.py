148616b69f69180146a8c73bf7594886555d9ef0
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-02 14:37:28 +0100

    print fizzbuzz if divisible by 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,10 @@
 class FizzBuzz:
     for x in range(1,101):
-        if x % 3 == 0:
+        if x % 15 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
             print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
         else:
             print(x)
